fb34b33a60353680b6cefa570ee84ce02f3f431f
Matt Hale <matthew.hale@protonmail.com>
2021-01-03 18:35:31 -0500

    Add subparsers

--- pf.py
+++ pf.py
@@ -16,20 +16,36 @@ pythfinder_dir = Path("{}/.pythfinder".format(Path.home()))
 def ensure_pythfinder_dir():
     Path(pythfinder_dir).mkdir(exist_ok=True)
 
+# Get various pieces of character information
+def get(args):
+    print("you called 'get' with the target: {}".format(args.target))
+
 # Argument parsing
-pythfinder_parser = argparse.ArgumentParser(description = "The pythfinder CLI")
+parser = argparse.ArgumentParser(description = "The pythfinder CLI")
 
-pythfinder_parser.add_argument("action", metavar = "action",
-                               help = "the action to be taken on the character sheet",
-                               choices = ["get"])
+subparsers = parser.add_subparsers(title = "actions")
 
-pythfinder_parser.add_argument("target", metavar = "target",
-                               help = "the character sheet you intend to interact with")
+parser_get = subparsers.add_parser("get",
+                                   help = "list various pieces of the character sheet")
+
+parser_get.add_argument("target", metavar = "target",
+                        help = "the element of the character sheet to 'get'",
+                        choices = ["character", "equipment"])
+
+parser_get.set_defaults(func = get)
+parser_save = subparsers.add_parser("save",
+                                    help = "save the character sheet in source to dest")
+
+parser_save.add_argument("source", metavar = "source")
+parser_save.add_argument("destination", metavar = "destination")
 
 if __name__ == "__main__":
-    ensure_pythfinder_dir()
+    # Parse args
     if len(sys.argv) < 2:
-        pythfinder_parser.print_help()
+        parser.print_help()
         sys.exit(0)
-    args = pythfinder_parser.parse_args()
-    print("{} {}".format(args.action, args.target))
+    args = parser.parse_args()
+
+    ensure_pythfinder_dir()
+
+    args.func(args)
