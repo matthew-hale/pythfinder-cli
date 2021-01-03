3084ce6c8acc07b57c9f6171273d087489cda488
Matt Hale <matthew.hale@protonmail.com>
2021-01-03 17:57:41 -0500

    Move stuff around

--- pf.py
+++ pf.py
@@ -9,18 +9,15 @@ import sys
 from pythfinder import Character
 from pathlib import Path
 
-"""
-pythfinder directory path, in $home
-"""
+# pythfinder directory path, in $home
 pythfinder_dir = Path("{}/.pythfinder".format(Path.home()))
 
-pythfinder_parser = argparse.ArgumentParser()
-
-"""
-Ensure that the pythfinder directory exists
-"""
+# Ensure that the pythfinder directory exists
 def ensure_pythfinder_dir():
     Path(pythfinder_dir).mkdir(exist_ok=True)
+
+# Argument parsing
+pythfinder_parser = argparse.ArgumentParser()
 
 if __name__ == "__main__":
     ensure_pythfinder_dir()
