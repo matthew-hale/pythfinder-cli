a7fb7da761a922ef5cc642a9f650cd55fe010c5b
Matt Hale <matthew.hale@protonmail.com>
2021-01-03 18:14:01 -0500

    Add simple debugging message for arg parsing

--- pf.py
+++ pf.py
@@ -17,8 +17,19 @@ def ensure_pythfinder_dir():
     Path(pythfinder_dir).mkdir(exist_ok=True)
 
 # Argument parsing
-pythfinder_parser = argparse.ArgumentParser()
+pythfinder_parser = argparse.ArgumentParser(description = "The pythfinder CLI")
+
+pythfinder_parser.add_argument("action", metavar = "action",
+                               help = "the action to be taken on the character sheet",
+                               choices = ["get"])
+
+pythfinder_parser.add_argument("target", metavar = "target",
+                               help = "the character sheet you intend to interact with")
 
 if __name__ == "__main__":
     ensure_pythfinder_dir()
-    pythfinder_parser.parse_args()
+    if len(sys.argv) < 2:
+        pythfinder_parser.print_help()
+        sys.exit(0)
+    args = pythfinder_parser.parse_args()
+    print("{} {}".format(args.action, args.target))
